ae08942773ae7787e8e1730d394e06265aa26235
N1ghtm <106837073+dn1ghtm@users.noreply.github.com>
2022-12-16 20:12:11 +0100

    Add files via upload

---
+++ main/js/javascript.js
@@ -0,0 +1,29 @@
+function checkPassword() {
+    var password = document.getElementById("text");
+    var passwordText = password.value;
+    var lenght = passwordText.lenght;
+    if(passwordText == "kjb3e96sw2") {
+     location = 'link';
+     noerror();
+ }else{
+     error();
+ }
+ if(passwordText == "uwu") {
+     location = 'pages/hi.html';
+     noerror();
+  }else{
+     if(passwordText == "kjb3e96sw2"){
+         noerror();
+     }
+     error();
+  }
+     }
+
+    function error(){
+     var p = document.getElementById("error");
+     p.classList.add("active");
+    }
+    function noerror(){
+     var pe = document.getElementById("error");
+     pe.classList.remove("active");
+    }
