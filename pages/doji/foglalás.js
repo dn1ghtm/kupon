7b8eb73fb7e0bc6ffcae78bb7ed2a0dbfc0ca35b
dn1ghtm <varjaszoltan@Varjas-MacBook-Pro.local>
2022-12-24 11:23:14 +0100

    notfinished

---
+++ pages/doji/foglalás.js
@@ -0,0 +1,10 @@
+const elsokartya = document.querySelector('grid-item');
+const elso = document.getElementsById("elso").addEventListener("click", function() {
+    alert("hi");
+})
+const masodik = document.getElementsById("2.");
+const harmadik = document.getElementById("3.");
+
+function teszt(){
+    elsokartya.classList.add('.hid');
+}
